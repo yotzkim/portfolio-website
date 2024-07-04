59913e43823a92bf38222824e8e880063c9426fe
Yoto Kim <yongtokim1215@gmail.com>
2024-07-04 16:54:44 -0700

    Email Section

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,11 @@ import Navbar from "./components/Navbar"
 import About from "./components/AboutMe"
 import AboutSection from "./components/AboutMe";
 import ProjectsSection from "./components/ProjectsSection";
+import EmailSection from "./components/EmailSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar/>
       <div class="container mx-auto px-12 py-20">
       <HeroSection />
@@ -16,6 +17,7 @@ export default function Home() {
       <div className="py-8"></div>
       <ProjectsSection />
       </div>
+      <EmailSection/>
     </main>
   );
 }
