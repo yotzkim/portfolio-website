e7fd9a62a06cbf3bf7a3d95eb86c28c53564983b
Yoto Kim <yongtokim1215@gmail.com>
2024-07-07 13:30:23 -0700

    Removed EmailSection traces

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
-import Image from "next/image";
+
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar"
-import About from "./components/AboutMe"
 import AboutSection from "./components/AboutMe";
 import ProjectsSection from "./components/ProjectsSection";
-import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 
 export default function Home() {
